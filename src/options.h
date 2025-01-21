945cc65a2e03c1bb0bb2c98dbf725348100d018b
TachyonRad-Michael <michael@tachyonrad.com>
2025-01-21 13:22:02 +0000

    Update options.h

--- src/options.h
+++ src/options.h
@@ -11,15 +11,15 @@
 
 // System Options
 #define RF_SX1278 "SX1278"
-#define RSSI_THRESHOLD 9
-#define OOK_FIXED_THRESHOLD 0x50
+#define RSSI_THRESHOLD 5
+#define OOK_FIXED_THRESHOLD 0x0C
 
 //reporting & debug options
-#define RF_MODULE_INIT_STATUS true
+//#define RF_MODULE_INIT_STATUS true
 //#define SIGNAL_RSSI true
 //#define RTL_DEBUG 4
 //#define RADIOLIB_DEBUG true
-#define RAW_SIGNAL_DEBUG true
+//#define RAW_SIGNAL_DEBUG true
 //#define DEMOD_DEBUG true
 //#define PUBLISH_UNPARSED true
 
